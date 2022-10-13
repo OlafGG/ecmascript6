6ed5344219b018313315ab2fe5b2f9a6156f664c
OlafGG <baezauriel01@gmail.com>
2022-10-13 11:40:16 -0500

    New changes to learn

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,3 +21,53 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+
+let lorem = "frase epica que se puede separar \n"
++ "otra frase epica"
+
+let lorem2 = `otra frase epica
+ahora otra frase epica
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+//Destructuracion de elementos
+
+let person = {
+    'name': 'Olaf',
+    'age': 21,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+let {name, age, country} = person;
+console.log(name, age);
+
+//operador de propagaci[on
+
+let team1 = ['Olaf', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+//Let es para el bloque de codigo: Var es global : const es una variable que no cambia
+
+{
+    var globalVar = "Global var"
+}
+
+{
+    let globalLet = 'Global Let'
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+
+const a = 'b';
+
