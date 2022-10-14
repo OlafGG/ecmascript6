4078990aa2ff9320cef22bb0405f1c0c95b5a6f0
OlafGG <baezauriel01@gmail.com>
2022-10-14 12:05:04 -0500

    new promise use, learn about how to make api call's

--- src/es6/index.js
+++ src/es6/index.js
@@ -71,3 +71,53 @@ console.log(globalVar);
 
 const a = 'b';
 
+//objetos mejorado
+{
+let name = 'Olaf';
+let age = 21;
+
+obj = {name: name, age: age};
+
+obj2 = {name, age}
+console.log(obj);
+console.log(obj2);
+}
+
+//funciones flecha
+
+const names = [
+    {name: 'Oscar', age: 21},
+    {name: 'Jessica', age: 27}
+]
+
+let listofnames = names.map(function(item) {
+    console.log(item.name);
+});
+
+let listofnames2 = names.map(item=>console.log(item.name));
+
+let listofnames3 = (name, age, country) => {
+    
+}
+
+const listofnames4 = name => {
+
+}
+
+const square = num => num * num;
+
+//promesas, investigar llamadas a APIS!
+const helloPromise = () =>{
+    return new Promise((resolve, reject)=>{
+        if(true){
+            resolve('Hey!');
+        }else{
+            reject('Ups!');
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('Hola'))
+    .catch(error => console.log(error));
